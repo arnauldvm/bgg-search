c993adabd01022a3851053f44399325bef3beb08
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2026-06-08 00:24:32 +0200

    upd(src/bgg_search/__init__.py): expose search_games and get_game in public API

--- src/bgg_search/__init__.py
+++ src/bgg_search/__init__.py
@@ -3,6 +3,7 @@ from importlib.metadata import version
 from bgg_search._protocol import BggClientProtocol
 from bgg_search.exceptions import BggApiError, BggNotFoundError, BggParseError, BggSearchError
 from bgg_search.models import GameDetails, GameSummary
+from bgg_search.search import get_game, search_games
 
 __version__ = version("bgg-search")
 
@@ -15,4 +16,6 @@ __all__ = [
     "BggSearchError",
     "GameDetails",
     "GameSummary",
+    "get_game",
+    "search_games",
 ]
