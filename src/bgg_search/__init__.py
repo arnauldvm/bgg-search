b6a885770abcccf1f3a71ce47128e9054d99e296
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2026-06-07 02:03:19 +0200

    feat(src/bgg_search/__init__.py): explicit public API surface with __all__

--- src/bgg_search/__init__.py
+++ src/bgg_search/__init__.py
@@ -1,3 +1,18 @@
 from importlib.metadata import version
 
+from bgg_search._protocol import BggClientProtocol
+from bgg_search.exceptions import BggApiError, BggNotFoundError, BggParseError, BggSearchError
+from bgg_search.models import GameDetails, GameSummary
+
 __version__ = version("bgg-search")
+
+__all__ = [
+    "__version__",
+    "BggClientProtocol",
+    "BggApiError",
+    "BggNotFoundError",
+    "BggParseError",
+    "BggSearchError",
+    "GameDetails",
+    "GameSummary",
+]
